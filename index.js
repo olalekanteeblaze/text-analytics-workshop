5cfa03434b898920339ef76da97ccd40b31e9ea7
Moshood Abidemi <olalekanteeblaze@gmail.com>
2021-06-17 23:22:57 +0100

    add entity recognition

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
-const sentimentAnalysis = require("./sentimentAnalysis");
+const entityRecognition = require("./entityRecognition");
 const key = '7b5e3e518b3c41e7805f41b46c48a99a'
 const endpoint = 'https://new-analytics-service.cognitiveservices.azure.com/'
 
 const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key));
 
-//Sentiment Analysis
-sentimentAnalysis(textAnalyticsClient)
+//Entity Recognition
+entityRecognition(textAnalyticsClient)
