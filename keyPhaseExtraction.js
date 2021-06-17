23a085d988d5a77497e3ed4f41ad7e25bae1e491
Moshood Abidemi <olalekanteeblaze@gmail.com>
2021-06-17 23:29:50 +0100

    add keyphrase extraction

---
+++ keyPhaseExtraction.js
@@ -0,0 +1,13 @@
+async function keyPhraseExtraction(client){
+
+    const keyPhrasesInput = [
+        "My cat might need to see a veterinarian.",
+    ];
+    const keyPhraseResult = await client.extractKeyPhrases(keyPhrasesInput);
+    
+    keyPhraseResult.forEach(document => {
+        console.log(`ID: ${document.id}`);
+        console.log(`\tDocument Key Phrases: ${document.keyPhrases}`);
+    });
+}
+module.exports = keyPhraseExtraction
