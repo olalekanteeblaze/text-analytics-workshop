67cf435f20a9907eb5739bc81ec705fc620ade5b
Moshood Abidemi <olalekanteeblaze@gmail.com>
2021-06-17 23:19:33 +0100

    add language detection

---
+++ languageDetection.js
@@ -0,0 +1,13 @@
+async function languageDetection(client) {
+
+    const languageInputArray = [
+        "Ce document est rédigé en Français."
+    ];
+    const languageResult = await client.detectLanguage(languageInputArray);
+
+    languageResult.forEach(document => {
+        console.log(`ID: ${document.id}`);
+        console.log(`\tPrimary Language ${document.primaryLanguage.name}`)
+    });
+}
+module.exports = languageDetection
